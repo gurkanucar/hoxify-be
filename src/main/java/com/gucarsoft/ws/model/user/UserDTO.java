50e6019496425c8e310178077e38fff63169c6da
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-05-28 15:59:54 +0300

    typo fixed

--- src/main/java/com/gucarsoft/ws/model/user/UserDTO.java
+++ src/main/java/com/gucarsoft/ws/model/user/UserDTO.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class UserDTO {
-    private String usernam;
+    private String username;
     private String name;
     private String image;
 
     public UserDTO(User user) {
-        this.usernam = user.getUsername();
+        this.username = user.getUsername();
         this.name =user.getName();
         this.image = user.getImage();
     }
